208d02bd1444140a85bee44816b1cd3078627d19
HelloAdrian2k <mz.adriaan@gmail.com>
2025-10-07 19:19:12 +0200

    refactor: quit check_path() function

--- modules/check.py
+++ modules/check.py
@@ -2,7 +2,7 @@ import os
 from config.settings import command_list
 
 def user_input():
-    return input('> ').split(' ')
+    return input('> ').strip().split(' ')
 
 def check_user_input(command):
   if not command[0] in command_list:
@@ -10,15 +10,19 @@ def check_user_input(command):
     return False
   return True
 
-def check_path(path):
-  if os.path.exists(path):
-    return True
-  print('\nERROR: Path not found')
-  return False
-
 def check_folder(path):
-  if check_path(path):
+  if os.path.exists(path):
     if os.path.isdir(path):
       return True
     print('\nERROR: Not a folder')
+  else:
+    print('\nERROR: Path not found')
   return False
+
+def valid_command_input(input_list, error_str = True):
+  list_ok = True
+  if not input_list: 
+    list_ok = False
+  if error_str and not list_ok:
+    print('ERROR: No command input')
+  return list_ok
